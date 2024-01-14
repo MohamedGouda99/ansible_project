f33afca8637ed6eff6e1ef06eabab1d97d394a14
MG <105832307+MohamedGouda99@users.noreply.github.com>
2024-01-14 22:42:31 +0200

    first commit

---
+++ project/main.tf
@@ -0,0 +1,126 @@
+resource "aws_s3_bucket" "terraform_state"{
+    bucket = "sprints-remote-statefile"
+    lifecycle {
+        prevent_destroy = false
+  } 
+}
+
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "sprints-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    "Name" = "${var.env_prefix}-subnet-1"
+
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+
+  }
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
+
+
+resource "aws_route_table_association" "myapp-ass-rtb-subnet" {
+  subnet_id      = aws_subnet.dev-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-key-pair"  # Change this to your desired key pair name
+  public_key = file("~/.ssh/id_rsa.pub")  # Change the path to your public key file
+}
+
+resource "aws_instance" "myapp-server" {
+  ami                         = "ami-0e5f882be1900e43b"
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.dev-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.my_key_pair.key_name
+
+  tags = {
+    Name = "rally_server"
+  }
+}
+

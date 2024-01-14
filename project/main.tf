1784d2865f5d938539bbd40e66d4e387ec383501
MG <105832307+MohamedGouda99@users.noreply.github.com>
2024-01-14 22:47:16 +0200

    2

--- project/main.tf
+++ project/main.tf
@@ -109,9 +109,24 @@ resource "aws_key_pair" "my_key_pair" {
   key_name   = "my-key-pair"  # Change this to your desired key pair name
   public_key = file("~/.ssh/id_rsa.pub")  # Change the path to your public key file
 }
+data "aws_ami" "ubuntu" {
 
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
 resource "aws_instance" "myapp-server" {
-  ami                         = "ami-0e5f882be1900e43b"
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.dev-subnet-1.id
   vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
